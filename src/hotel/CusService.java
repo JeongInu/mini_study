25f416f29e040a414ade4b07459e442570a646c0
kimddo1 <dowonkim2004@gmail.com>
2025-03-16 00:00:41 +0900

    불필요한 Main 삭제, Customer & CusService 첫 수정

--- src/hotel/CusService.java
+++ src/hotel/CusService.java
@@ -1,4 +1,37 @@
 package hotel;
 
+import java.util.LinkedList;
+
 public class CusService {
+    private static LinkedList<Customer> cusQueue = new LinkedList<>();
+    private String name;
+    private String phoneNo;
+    
+    public CusService(){}
+
+    public void makeReserv(){
+        System.out.print("이름: ");
+        name = DataInput.sc.nextLine();
+
+        while (true) {
+            System.out.print("전화번호: ");
+            phoneNo = DataInput.sc.nextLine();
+            if(phoneNo.matches("^\\d{3}-\\d{4}-\\d{4}$")){
+                cusQueue.offer(new Customer(name,phoneNo));
+                break;
+            } else {
+                System.out.println("000-0000-0000 형식으로 기입하시오");
+            }
+        }
+        
+
+    }
+
+    public void show(){
+        for(int i=0;i<cusQueue.size();i++){
+            System.out.println(cusQueue.get(i).getName());
+            System.out.println(cusQueue.get(i).getNumber());
+        }
+    }
+
 }
--- src/hotel/Customer.java
+++ src/hotel/Customer.java
@@ -4,5 +4,31 @@ public class Customer {
 
     private String name;
     private String number;
+    public Object getName;
+
+    public Customer() {}
+
+    public Customer(String name, String number) {
+        super();
+        this.name = name;
+        this.number = number;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 
 }
